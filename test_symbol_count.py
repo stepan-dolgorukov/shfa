6b4d067679267bcac267a89f4dcd772d9d2328f1
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-04-13 00:05:42 +0500

    Усиление тестов SymbolCountMap

--- test_symbol_count.py
+++ test_symbol_count.py
@@ -10,14 +10,36 @@ class TestSymbolCountMap(unittest.TestCase):
         s = ''
         m = SymbolCountMap(s)
 
+        self.assertEqual(0, len(m))
+        self.assertEqual(0, len(m.keys()))
+        self.assertEqual(0, len(m.values()))
         self.assertEqual(0, len(m.items()))
-        self.assertEqual(-1, m.at(chr(0)))
-        self.assertEqual(-1, m.at('a'))
+
+        # chr(i) определена на 0 <= i <= 0x10ffff
+        for char_code in range(0, 0x10ffff + 1):
+            char = chr(char_code)
+            self.assertEqual(-1, m.at(char))
 
     def test_upper_and_lower(self):
         """Тест на обработку заглавных и строчных символов."""
         s = 'aAbBYyZwzWaA'
         m = SymbolCountMap(s)
+
+        # a: 2
+        # A: 2
+        # b: 1
+        # B: 1
+        # Y: 1
+        # y: 1
+        # Z: 1
+        # z: 1
+        # w: 1
+        # W: 1
+
+        self.assertEqual(10, len(m))
+        self.assertEqual(10, len(m.keys()))
+        self.assertEqual(10, len(m.values()))
+        self.assertEqual(10, len(m.items()))
 
         self.assertEqual(2, m.at('a'))
         self.assertEqual(2, m.at('A'))
@@ -29,6 +51,12 @@ class TestSymbolCountMap(unittest.TestCase):
         self.assertEqual(1, m.at('Z'))
         self.assertEqual(1, m.at('w'))
         self.assertEqual(1, m.at('W'))
+
+        # chr(i) определена на 0 <= i <= 0x10ffff
+        for char_code in range(0, 0x10ffff + 1):
+            char = chr(char_code)
+            if char not in s:
+                self.assertEqual(-1, m.at(char))
 
 
 if __name__ == '__main__':
