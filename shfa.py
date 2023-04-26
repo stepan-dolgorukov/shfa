ba2ed426370db35ebb3b715ebdf2ca0297dc5655
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-04-26 12:12:17 +0500

    Информирование пользователя о причине невозможности обработки

--- shfa.py
+++ shfa.py
@@ -8,20 +8,26 @@ import os.path
 def parse_args():
     parser = argparse.ArgumentParser(prog="Архиватор «Shannon-Fano»")
 
-    parser.add_argument("--filename", "-f")
-    parser.add_argument("--action", "-a")
+    parser.add_argument("--filename", "-f", nargs='?')
+    parser.add_argument("--action", "-a", nargs='?')
 
     args = parser.parse_args()
     return args
 
 def check_args(args):
-    if (args.filename is None) or (not os.path.exists(args.filename)):
-        return False
+    if args.filename is None:
+        return "Не указано имя файла"
 
-    if (args.action is None) or (args.action not in {'e', 'd'}):
-        return False
+    if not os.path.exists(args.filename):
+        return "Файл не существует"
 
-    return True
+    if args.action is None:
+        return "Не указано действие"
+
+    if args.action not in {'e', 'd'}:
+        return "Указано некорректное действие"
+
+    return "Хорошо"
 
 def encode(filename):
     data = ""
@@ -40,8 +46,10 @@ def decode(filename):
 
 if __name__ == '__main__':
     args = parse_args()
+    message = check_args(args)
 
-    if not check_args(args):
+    if "Хорошо" != message:
+        print(message)
         exit()
 
     if args.action == 'e':
