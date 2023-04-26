b402703e38aa055fd4459f8ed62516a1686fbe21
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-04-26 09:40:04 +0500

    Основное взаимодействие с пользователем

--- shfa.py
+++ shfa.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 
-from symbol_code import SymbolCodeMap
+from writer import CompressionWriter
+from reader import DecompressionReader
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog="Архиватор «Shannon-Fano»")
+
+    parser.add_argument("--filename", "-f")
+    parser.add_argument("--action", "-a")
+
+    args = parser.parse_args()
+    return args
+
+def encode(filename):
+    data = ""
+    with open(filename) as file:
+        data = file.readline()
+
+    writer = CompressionWriter(data, filename + '.compressed')
+    writer.write()
+
+def decode(filename):
+    reader = DecompressionReader(filename)
+    data = reader.read()
+
+    print(data)
+    return data
 
 if __name__ == '__main__':
-    symbol_code = SymbolCodeMap("Hello world!")
-    for symbol in symbol_code:
-        print(symbol, symbol_code.at(symbol))
+    data = ""
+    args = parse_args()
+
+    if args.action == 'e':
+        encode(args.filename)
+
+    if args.action == 'd':
+        decode(args.filename)
