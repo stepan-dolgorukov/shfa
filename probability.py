704d486676b8a2d746ad7530d28791e72fbac61a
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-11 17:38:24 +0500

    Удаление неиспользуемого словаря

--- probability.py
+++ probability.py
@@ -1,6 +1,5 @@
 from collections import Counter
 
 def byte_probability(data: bytes):
-    probability = {}
-    byte_count = Counter(data)
+    byte_count  = Counter(data)
     return {byte: byte_count[byte] / len(data) for byte in byte_count}
