8c74312a4cc8fe46cfa022101af72e73ec5998a7
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-12 10:23:09 +0500

    Проверки аргумента «data»

--- probability.py
+++ probability.py
@@ -1,5 +1,11 @@
 from collections import Counter
 
 def byte_probability(data: bytes):
+    if not isinstance(data, bytes):
+        raise ValueError("Допустимы только байтовые строки")
+
+    if not data:
+        raise ValueError("Пустая строка байтов")
+
     byte_count  = Counter(data)
     return {byte: byte_count[byte] / len(data) for byte in byte_count}
