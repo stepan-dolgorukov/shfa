92fd1323d6d67a408119c7966c2877410342a925
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-19 12:17:07 +0500

    Базовая натяжка {en,de}code в ShannonFanoApplication

--- shfa_gui.py
+++ shfa_gui.py
@@ -6,91 +6,6 @@ from tkinter.filedialog import askopenfilename
 from writer import CompressionWriter
 from reader import DecompressionReader
 
-def encode():
-    filename = askopenfilename()
-
-    if not filename:
-        tkinter.messagebox.showerror(title="Ошибка", message="Файл не выбран")
-        return
-
-    inp=tkinter.Tk()
-    inp.title("Файл вывода")
-
-    data = ""
-    with open(filename, 'rb') as file:
-        data = file.read()
-
-    e = tkinter.Entry(inp, text='Укажите название файла')
-    e.grid(row=0,column=0)
-    e.focus()
-
-    def on_pressed(output):
-
-        try:
-            writer = CompressionWriter(data, output)
-        except Exception as exc:
-            tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            inp.destroy()
-            return
-
-        try:
-            writer.write()
-        except Exception as exc:
-            tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            inp.destroy()
-            return
-
-        tkinter.messagebox.showinfo(title="Успешно",
-            message=f"Сжатая информация записана в {output}")
-
-        inp.destroy()
-
-
-    b = tkinter.Button(inp, text='Sub',command=lambda: on_pressed(e.get()))
-    b.grid(row=0, column=1)
-
-
-def decode():
-    filename = askopenfilename()
-
-    if not filename:
-        tkinter.messagebox.showerror(title="Ошибка", message="Файл не выбран")
-        return
-
-    inp=tkinter.Tk()
-    inp.title("Файл вывода")
-
-    e = tkinter.Entry(inp, text="Укажите имя файла")
-    e.grid(row=0,column=0)
-    e.focus()
-
-    def on_pressed(output):
-        data = None
-
-        try:
-            reader = DecompressionReader(filename)
-            data = reader.read()
-        except Exception as exc:
-            tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            inp.destroy()
-            return
-
-        try:
-            with open(output, "wb") as out:
-                out.write(data)
-        except Exception as exc:
-            tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            inp.destroy()
-            return
-
-        tkinter.messagebox.showinfo(title="Успешно",
-            message=f"Расжатая информация записана в {output}")
-
-        inp.destroy()
-
-    b = tkinter.Button(inp, text='Sub',command=lambda: on_pressed(e.get()))
-    b.grid(row=0, column=1)
-
 
 class ShannonFanoApplication:
     def __init__(self):
@@ -100,11 +15,95 @@ class ShannonFanoApplication:
         frm = ttk.Frame(self.root, padding=10)
         frm.grid()
 
-        ttk.Button(frm, text="Enc", command=encode).grid(column=0, row=0)
-        ttk.Button(frm, text="Dec", command=decode).grid(column=1, row=0)
+        ttk.Button(frm, text="Enc", command=self.encode).grid(column=0, row=0)
+        ttk.Button(frm, text="Dec", command=self.decode).grid(column=1, row=0)
 
     def start(self):
         self.root.mainloop()
+
+    def encode(self):
+        filename = askopenfilename()
+
+        if not filename:
+            tkinter.messagebox.showerror(title="Ошибка", message="Файл не выбран")
+            return
+
+        inp=tkinter.Tk()
+        inp.title("Файл вывода")
+
+        data = ""
+        with open(filename, 'rb') as file:
+            data = file.read()
+
+        e = tkinter.Entry(inp, text='Укажите название файла')
+        e.grid(row=0,column=0)
+        e.focus()
+
+        def on_pressed(output):
+
+            try:
+                writer = CompressionWriter(data, output)
+            except Exception as exc:
+                tkinter.messagebox.showerror(title="Ошибка", message=exc)
+                inp.destroy()
+                return
+
+            try:
+                writer.write()
+            except Exception as exc:
+                tkinter.messagebox.showerror(title="Ошибка", message=exc)
+                inp.destroy()
+                return
+
+            tkinter.messagebox.showinfo(title="Успешно",
+                message=f"Сжатая информация записана в {output}")
+
+            inp.destroy()
+
+
+        b = tkinter.Button(inp, text='Sub',command=lambda: on_pressed(e.get()))
+        b.grid(row=0, column=1)
+
+    def decode(self):
+        filename = askopenfilename()
+
+        if not filename:
+            tkinter.messagebox.showerror(title="Ошибка", message="Файл не выбран")
+            return
+
+        inp=tkinter.Tk()
+        inp.title("Файл вывода")
+
+        e = tkinter.Entry(inp, text="Укажите имя файла")
+        e.grid(row=0,column=0)
+        e.focus()
+
+        def on_pressed(output):
+            data = None
+
+            try:
+                reader = DecompressionReader(filename)
+                data = reader.read()
+            except Exception as exc:
+                tkinter.messagebox.showerror(title="Ошибка", message=exc)
+                inp.destroy()
+                return
+
+            try:
+                with open(output, "wb") as out:
+                    out.write(data)
+            except Exception as exc:
+                tkinter.messagebox.showerror(title="Ошибка", message=exc)
+                inp.destroy()
+                return
+
+            tkinter.messagebox.showinfo(title="Успешно",
+                message=f"Расжатая информация записана в {output}")
+
+            inp.destroy()
+
+        b = tkinter.Button(inp, text='Sub',command=lambda: on_pressed(e.get()))
+        b.grid(row=0, column=1)
 
 
 if __name__ == '__main__':
