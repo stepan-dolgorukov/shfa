1f32c512f9f1498752dcc07b77d943765db380c8
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-21 18:55:36 +0500

    Тестирование writer.Writer

--- test/test_writer.py
+++ test/test_writer.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import Mock
 from bitstring import BitArray
-from writer import CompressionWriter
+from writer import CompressionWriter, Writer
 
 
 class TestCompressionWriter(unittest.TestCase):
@@ -19,6 +19,25 @@ class TestCompressionWriter(unittest.TestCase):
     def test_filename_empty(self):
         self.assertRaises(ValueError, CompressionWriter, b'Hello', str())
 
+class TestWriter(unittest.TestCase):
+    def test_data_wrong_type(self):
+        for data in (str(), int(), dict(), set()):
+            self.assertRaises(TypeError, Writer, data, 'input')
+
+    def test_data_empty(self):
+        self.assertRaises(ValueError, Writer, b'', 'input')
+
+    def test_filename_wrong_type(self):
+        for filename in (bytes(), int(), dict(), set()):
+            self.assertRaises(TypeError, Writer, b'Hello', filename)
+
+    def test_correct_arguments(self):
+        Writer.file_exists = Mock(return_value=False)
+
+        try:
+            Writer(b"Hello", 'input')
+        except Exception:
+            self.fail()
 
 if __name__ == '__main__':
     unittest.main()
