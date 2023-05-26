cc14e82ce799c7dbc1a496e9f07ca6bf159adaef
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-26 22:41:03 +0500

    Окошко выбора файла вывода

--- shfa_gui.py
+++ shfa_gui.py
@@ -15,55 +15,44 @@ class EncodeButton:
         button = ttk.Button(
             root_frame,
             text="Закодировать",
-            command=self.encode)
+            command=self.on_press)
         button.grid(row=0, column=0)
 
-    def encode(self):
+    def on_press(self):
         """Обработка нажатия кнопки."""
 
-        self.filename = askopenfilename()
+        filename = askopenfilename(
+            title="Выбор файла для закодирования"
+        )
 
-        if not self.filename:
+        if not filename:
             return
 
-        self.output_file_window = tkinter.Toplevel(self.root)
-        self.output_file_window.title("Файл вывода")
-        self.output_file_window.geometry("200x50")
+        output = tkinter.filedialog.asksaveasfilename(
+            title="Выбор файла для сохранения",
+            confirmoverwrite=False
+        )
 
-        output_file_entry = tkinter.Entry(
-            master=self.output_file_window,
-            text='Укажите название файла')
-        output_file_entry.grid(row=0, column=0)
-        output_file_entry.focus()
-        output_file_entry.bind("<Return>", self.on_return)
-
-    def on_return(self, event):
-        """Обработка ввода имени файла выхода."""
-
-        output = event.widget.get()
+        if not output:
+            return
 
         try:
-            reader = Reader(self.filename)
+            reader = Reader(filename)
             data = reader.read()
 
             writer = CompressionWriter(data, output)
         except Exception as exc:
             tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            self.output_file_window.destroy()
             return
 
         try:
             writer.write()
         except Exception as exc:
             tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            self.output_file_window.destroy()
             return
 
         tkinter.messagebox.showinfo(title="Успешно",
                                     message=f"Сжатая информация записана в {output}")
-
-        self.output_file_window.destroy()
-
 
 class DecodeButton:
     """Кнопка декодирования файла."""
@@ -73,41 +62,32 @@ class DecodeButton:
         button = ttk.Button(
             root_frame,
             text="Декодировать",
-            command=self.decode)
+            command=self.on_press)
         button.grid(row=1, column=0)
 
-    def decode(self):
+    def on_press(self):
         """Обработка нажатия на кнопку."""
 
-        self.filename = askopenfilename()
+        filename = askopenfilename()
 
-        if not self.filename:
+        if not filename:
             return
 
-        self.output_file_input_window = tkinter.Toplevel(self.root)
-        self.output_file_input_window.title("Файл вывода")
-        self.output_file_input_window.geometry("200x50")
+        output = tkinter.filedialog.asksaveasfilename(
+            title="Выбор файла для сохранения",
+            confirmoverwrite=False
+        )
 
-        output_file_entry = tkinter.Entry(
-            self.output_file_input_window,
-            text="Укажите имя файла")
-        output_file_entry.grid(row=0, column=0)
-        output_file_entry.focus()
+        if not output:
+            return
 
-        output_file_entry.bind("<Return>", self.on_return)
-
-    def on_return(self, event):
-        """Обработка ввода пользователем имени файла вывода."""
-
-        output = event.widget.get()
         data = None
 
         try:
-            reader = DecompressionReader(self.filename)
+            reader = DecompressionReader(filename)
             data = reader.read()
         except Exception as exc:
             tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            self.output_file_input_window.destroy()
             return
 
         try:
@@ -115,13 +95,10 @@ class DecodeButton:
             writer.write()
         except Exception as exc:
             tkinter.messagebox.showerror(title="Ошибка", message=exc)
-            self.output_file_input_window.destroy()
             return
 
         tkinter.messagebox.showinfo(title="Успешно",
                                     message=f"Расжатая информация записана в {output}")
-
-        self.output_file_input_window.destroy()
 
 
 class ShannonFanoApplication(tkinter.Frame):
