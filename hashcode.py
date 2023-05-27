371d77c48605b7d61470fcfc63d97ebf0e5e97a0
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-27 10:49:06 +0500

    Doc-строки

--- hashcode.py
+++ hashcode.py
@@ -1,6 +1,11 @@
 import hashlib
 
 def hashcode(data: bytes) -> str:
+    """Вычисление хэш-кода указанной байтовой строки.
+
+    data -- информация, хэш-код которой требуется вычислить.
+    """
+
     if not isinstance(data, bytes):
         raise TypeError("Информация должна быть типа «bytes»")
 
