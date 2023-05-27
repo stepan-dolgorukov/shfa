46b1f91bc0610196ef7b23221c1acc3bc376c0d3
Stёpa Dolgorukov <63650851+stepan-dolgorukov@users.noreply.github.com>
2023-05-27 15:38:20 +0500

    Модуль проверки имени файла на корректность

---
+++ nice_filename.py
@@ -0,0 +1,12 @@
+class NotNiceFileName(Exception):
+    """Исключение «Не хорошее имя файла»."""
+
+
+def file_name_is_nice(name: str):
+    if not isinstance(name, str):
+        return False
+
+    if not name:
+        return False
+
+    return True
